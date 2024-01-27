8f2be00d0010c0bd5b647828c6503474c433fdef
Maxwell Anselm <manselm@lutron.com>
2024-01-27 12:00:10 -0500

    basic menu

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
